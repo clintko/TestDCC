926bd14dec1039a77a3d45b4d1d16c84fefffc6f
clintko <clint00724@gmail.com>
2019-04-19 01:11:32 -0400

    remove shortread installation

--- mytestRpkg.R
+++ mytestRpkg.R
@@ -3,18 +3,12 @@ myCRAN <-'http://archive.linux.duke.edu/cran/'
 mypkgs<-c('devtools')
 install.packages(mypkgs, repos=myCRAN)
 
-### SETTING ShortRead
-if (!requireNamespace("BiocManager", quietly = TRUE))
-    install.packages("BiocManager")
-BiocManager::install("ShortRead", version = "3.8")
-
 ### Install my test packages
 devtools::install_github("clintko/TestDCCRpkg")
 library("TestDCCRpkg")
 
 ### try some functions
 hello()
-
 
 ### record the environment and exit
 sessionInfo()
