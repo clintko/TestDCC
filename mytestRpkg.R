118b2f0103d953d6688cb12092c844f793975919
clintko <clint00724@gmail.com>
2019-04-18 22:54:38 -0400

    add install shortread

--- mytestRpkg.R
+++ mytestRpkg.R
@@ -1,12 +1,21 @@
+### SETTING Devtools
 myCRAN <-'http://archive.linux.duke.edu/cran/'
-
 mypkgs<-c('devtools')
 install.packages(mypkgs, repos=myCRAN)
+
+### SETTING ShortRead
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install("ShortRead", version = "3.8")
+
+### Install my test packages
 devtools::install_github("clintko/TestDCCRpkg")
 library("TestDCCRpkg")
 
+### try some functions
 hello()
 
-sessionInfo()
 
+### record the environment and exit
+sessionInfo()
 q(save="no")
