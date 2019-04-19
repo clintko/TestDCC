c13146d139e8982a8f50a9c093efba84c3a041fa
clintko <clint00724@gmail.com>
2019-04-18 21:11:49 -0400

    add the call to the function hello

--- mytestRpkg.R
+++ mytestRpkg.R
@@ -3,5 +3,8 @@ myCRAN <-'http://archive.linux.duke.edu/cran/'
 mypkgs<-c('devtools')
 install.packages(mypkgs, repos=myCRAN)
 devtools::install_github("clintko/TestDCCRpkg")
+library("TestDCCRpkg")
+
+hello()
 
 sessionInfo()
