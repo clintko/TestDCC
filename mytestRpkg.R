299871addca063fc487d3407a87bb4e0c7120001
clintko <clint00724@gmail.com>
2019-04-18 21:51:24 -0400

    add back the code to install packages

--- mytestRpkg.R
+++ mytestRpkg.R
@@ -1,11 +1,11 @@
 myCRAN <-'http://archive.linux.duke.edu/cran/'
 
-#mypkgs<-c('devtools')
-#install.packages(mypkgs, repos=myCRAN)
-#devtools::install_github("clintko/TestDCCRpkg")
-#library("TestDCCRpkg")
+mypkgs<-c('devtools')
+install.packages(mypkgs, repos=myCRAN)
+devtools::install_github("clintko/TestDCCRpkg")
+library("TestDCCRpkg")
 
-for (x in 1:10){print(x)}
+hello()
 
 sessionInfo()
 
